26356fae8f4110b89d019705fed0d7aa08f70bda
Lisa <lduheron@e3r8p4.42.fr>
2022-12-02 17:13:38 +0100

    safety commit

--- GetNextLine/get_next_line.c
+++ GetNextLine/get_next_line.c
@@ -6,33 +6,86 @@
 /*   By: lduheron <lduheron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 13:06:11 by lduheron          #+#    #+#             */
-/*   Updated: 2022/11/30 14:52:59 by lduheron         ###   ########.fr       */
+/*   Updated: 2022/11/30 15:48:26 by lduheron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_calloc(size_t nmemb, size_t size);
+ssize_t	read(int fd, void *buf, size_t count);
+
+
 #include "get_next_line.h"
-#include <unistd.h>
 //fonctions autorisees read, malloc, free
- 
-
-
-static char	read_and_stock(int fd)
+static char	read_and_fulfill(int fd)
 {
-	int i;
-	int	line;
-
+	size_t 	i;
+	char	*line;
+	char	*buffertmp;
+	char	find_end_line // trouver le \n
+	
 	i = 0;
-	line = substr(start, strchr(fd, \n));
+	while (find_end_line = ft_strchr(line, \n) == 0)
+	while (line[i] != find_end_line)
+		{
+	
+		}
+		substr(start, strchr(line, \n));
+	
+	//if after le pointeur sur strch va dans remains
 //	read(fd, buff, BUFFER_SIZE);
-
+		line = strjoin(line, buffertmp)
+	return(line);
 }
 
+
+
+{
+	char	line;
+	char	*buffer;
+	char	*remains;
+
+	*buffer = calloc(buffer_size + 1), sizeof(char);
+	if (buffer == NULL)
+		return ;
+	
+}
 
 char *get_next_line(int fd)
 {
+	int	i;
+	int	x;
+
+	if (fd < 0 || buffer_size < 0)
+		return ;
+	*line = calloc(buffer_size + 1), sizeof(char);
+	*buffer = calloc(buffer_size + 1), sizeof(char);
+	if ( !line || !buffer)
+		return ;
+	
+	if (remains == NULL)
+	line = (strjoin )
+
+
+int	main(int buffer_size)
+{
+	int		fd;
+	size_t	i;
+	char	*line;
+	
+	i = 0;
+	fd = open(fd, RDONLY);
+	while (i >)
+	{
+		line = get_next_line(fd, buffersize);
+		printf;
+		i++;
+	}
 }
 
 
+/*
 
 while (i);
 (
@@ -48,22 +101,6 @@ line =
 buffer = 
 
 
-int	main()
-{
-	char	line;
-	char	*buffer;
-	int	remains
-
-
-	fd = open(fd, RDONLY)
-	buffer = calloc(buffer + 1)
-	if (buffer == NULL)
-		return ;
-	
-}
-
-
-/*
 size_t	ft_strlcpy(char *dst, const char *src, unsigned int size)
 {
 	size_t	i;
--- GetNextLine/get_next_line.h
+++ GetNextLine/get_next_line.h
@@ -1,7 +1,7 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h> //pour la fonction open
+# include <unistd.h> //pour les fonctions open et read
 # include <string.h> // pour la fonction strchr
 
 
@@ -15,5 +15,7 @@ char    *get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 void	*ft_calloc(size_t nmemb, size_t size);
+ssize_t read(int fd, void *buf, size_t count)
+
 
 #endif
